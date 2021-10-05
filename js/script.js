929b8eba3ace0448c264341138b14aa75bdbb070
BigMuscleMann <zanekg123@gmail.com>
2021-10-04 21:27:03 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@ function popUp(text, header) {
     mainPopup.style.height = window.innerHeight * .9;
     mainPopup.style.width = window.innerHeight * .9;
     main.style.filter = 'blur(3px)';
-
+    main.style.userSelect = 'none';
 }
