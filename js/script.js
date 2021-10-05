b6822fb863b08cd2c1338b7e80e52b0b728ebf97
BigMuscleMann <83908895+BigMuscleMann@users.noreply.github.com>
2021-10-05 10:44:04 -0500

    add html for popup

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ var main = document.getElementById('mainContainer')
 function popUp(text, header, toggle) {
 
     mainPopup = document.getElementById('popUpDiv');
+    mainPopupHeader = document.getElementById('popUpHeader');
+    mainPopupText = document.getElementById('popUpText');
 
     mainPopup.style.height = window.innerHeight * .9;
     mainPopup.style.width = window.innerHeight * .9;
@@ -18,4 +20,7 @@ function popUp(text, header, toggle) {
         mainPopup.style.display = 'none';
         main.style.userSelect = 'none';
     }
+
+
+
 }
