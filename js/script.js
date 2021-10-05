c35734fb5fbd95448e1e369541d46a575e853803
BigMuscleMann <zanekg123@gmail.com>
2021-10-04 21:07:37 -0500

    start popup function

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var main = document.getElementById('mainContainer')
+
+function popUp(text, header) {
+    mainPopup = document.getElementById('popUpDiv');
+    mainPopup.style.display = 'block';
+    mainPopup.style.height = window.innerHeight * .9;
+    mainPopup.style.width = window.innerHeight * .9;
+    main.style.filter = 'blur(3px)';
+
+}
